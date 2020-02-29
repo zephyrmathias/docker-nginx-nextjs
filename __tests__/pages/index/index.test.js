b463614dd843a8580b9c8c20c95527438b943fad
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-03-01 03:26:54 +0700

    setup jest and add basic unit test

---
+++ __tests__/pages/index/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Homepage from '../../../pages/index'
+
+describe('snapshot', () => {
+  it('should match with snapshot', () => {
+    const component = shallow(<Homepage />)
+    expect(component).toMatchSnapshot()
+  })
+})
