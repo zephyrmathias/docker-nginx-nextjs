8b126489c12879dc03340803c39d4384a8ea85c5
Wuttichai Junsoontraporn <wuttichai.junsoontraporn@gmail.com>
2020-03-08 00:00:41 +0700

    test(styled): added test for styled-components

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
       ctx.renderPage = () =>
         originalRenderPage({
           // eslint-disable-next-line react/jsx-props-no-spreading
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
         })
       const initialProps = await Document.getInitialProps(ctx)
 
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </React.Fragment>
-        )
+        ),
       }
     } finally {
       sheet.seal()
